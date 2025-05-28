2106c706084ad4367344c35d508209c87457b469
Frederik Madsen <frederik.meisner@gmail.com>
2025-05-28 12:29:08 +0200

    Improved contacts

--- src/sf_contact.ts
+++ src/sf_contact.ts
@@ -5,18 +5,29 @@ import { customElement } from "lit/decorators.js";
 export class sf_contact extends LitElement {
     renderRoot = this;
 
+    contact = [
+        { n: 'Frederik', tlf: '20724383', email: 'frederik.meisner@gmail.com' },
+        { n: 'Nikoline', tlf: '51927986', email: 'nikoline.petersen@live.com' },
+        { n: 'Sif', tlf: '', email: '' },
+        { n: 'Michael', tlf: '', email: '' },
+        { n: 'Janne', tlf: '31228832', email: 'janne_meisner@hotmail.com' },
+        { n: 'Morten', tlf: '', email: '' },
+    ]
+
+    render_contacts() {
+        return this.contact.map(c =>
+            html`
+            <li>${c.n}: ${c.tlf} <a href="mailto:${c.email}">${c.email}</a></li>
+        `);
+    }
+
     render() {
         return html`
             <div class="sf-content">
                 <h3>Kontakt</h3>
                 <p>Har du spørgsmål eller kommentarer, så kontakt os venligst.</p>
                 <ul>
-                    <li>Frederik. Telefon: 20724393 <a href="mailto:frederik.meisner@gmail.com">frederik.meisner@gmail.com</a> </li>
-                    <li>Nikoline. Telefon: 51927986 <a href="mailto:nikoline.petersen@live.com">nikoline.petersen@live.com</a> </li>
-                    <li>Sif </li>
-                    <li>Michael </li>
-                    <li>Janne. Telefon: 31228832 </li>
-                    <li>Morten </li>
+                    ${this.render_contacts()}
                 </ul>
             </div>
         `;
