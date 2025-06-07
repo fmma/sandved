32fbd8859a0819e141177f658f1ea00741ecece6
frma <frederik.meisner@gmail.com>
2025-06-07 17:49:05 +0200

    ting

--- src/sf_program.ts
+++ src/sf_program.ts
@@ -6,74 +6,75 @@ export class sf_program extends LitElement {
     renderRoot = this;
 
     render() {
-
         return html`
-            <div class="sf-content">
-                
+            <div class="sf-content sf-content-program">
                 <div class="sf-music-program">
                     <h3>Program</h3>
-                    <p>
-                    Her kommer programmet.
-                    </p>
-                    Torsdag<br><br>
-                    07:00 - 09:00: Morgenmad <br>
-                    12:00: Frokost <br>
-                    18:00: Aftensmad <br>
-                    <br>
-                    Fredag<br><br>
-                    07:00 - 09:00: Morgenmad <br>
-                    12:00: Frokost <br>
-                    18:00: Aftensmad <br>
-                    <br>
-                    Lørdag<br><br>
-                    07:00 - 09:00: Morgenmad <br>
-                    12:00: Frokost <br>
-                    18:00: Aftensmad <br>
-                    <br>
-                    Søndag<br><br>
-                    08:00 - 09:30: Morgenmad <br>
-
+                    <table class="sf-program-table">
+                        <thead>
+                            <tr>
+                                <th>Dag</th>
+                                <th>Tid</th>
+                                <th>Aktivitet</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                              <td class="sf-program-day" rowspan="3"><span class="sf-program-day">Torsdag</span></td>
+                              <td class="sf-program-time">08:00 - 09:30</td>
+                              <td>Morgenmad</td>
+                            </tr>
+                            <tr>
+                              <td class="sf-program-time">12:00 - 13:00</td>
+                              <td>Frokost</td>
+                            </tr>
+                            <tr>
+                              <td class="sf-program-time">18:00 - 19:00</td>
+                              <td>Aftensmad</td>
+                            </tr>
+                            <tr>
+                              <td class="sf-program-day" rowspan="4"><span class="sf-program-day">Fredag</span></td>
+                              <td class="sf-program-time">08:00 - 09:30</td>
+                              <td>Morgenmad</td>
+                            </tr>
+                            <tr>
+                              <td class="sf-program-time">12:00 - 13:00</td>
+                              <td>Frokost</td>
+                            </tr>
+                            <tr>
+                              <td class="sf-program-time">18:00 - 19:00</td>
+                              <td>Aftensmad</td>
+                            </tr>
+                            <tr>
+                              <td class="sf-program-time">20:00 - 21:00</td>
+                              <td><span class="sf-scene">[Store Scene]</span> Michaels Band</td>
+                            </tr>
+                            <tr>
+                              <td class="sf-program-day" rowspan="4"><span class="sf-program-day">Lørdag</span></td>
+                              <td class="sf-program-time">08:00 - 09:30</td>
+                              <td>Morgenmad</td>
+                            </tr>
+                            <tr>
+                              <td class="sf-program-time">12:00 - 13:00</td>
+                              <td>Frokost</td>
+                            </tr>
+                            <tr>
+                              <td class="sf-program-time">18:00 - 19:00</td>
+                              <td>Aftensmad</td>
+                            </tr>
+                            <tr>
+                              <td class="sf-program-time">03:00 - 04:00</td>
+                              <td>Digtoplæsning</td>
+                            </tr>
+                            <tr>
+                              <td class="sf-program-day"><span class="sf-program-day">Søndag</span></td>
+                              <td class="sf-program-time">08:00 - 09:30</td>
+                              <td>Morgenmad</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         `;
-
-        // return html`
-        //     <div class="sf-content">
-        //         <div class="sf-music-program">
-        //             <h3>Musikprogram</h3>
-        //             Torsdag<br><br>
-        //             07:00 - 09:00: Morgenmad <br>
-        //             09:00 - 11:00: AKTIVITET<br>
-        //             11:00 - 13:00: Frokost <br>
-        //             13:00 - 15:00: AKTIVITET<br>
-        //             17:30 - 19:00: Aftensmad <br>
-        //             19:00 - 20:00: <span class="sf-scene">[Lille Scene]</span> Sange i Solen med Nikoline <br>
-        //             20:30 - 22:00: <span class="sf-scene">[Store Scene]</span> Jam session <br>
-        //             <br>
-        //             Fredag<br><br>
-        //             07:00 - 09:00: Morgenmad <br>
-        //             09:00 - 11:00: AKTIVITET<br>
-        //             11:00 - 13:00: Frokost <br>
-        //             13:00 - 15:00: AKTIVITET<br>
-        //             17:30 - 19:00: Aftensmad <br>
-        //             19:00 - 20:00: <span class="sf-scene">[Store Scene]</span> Sandved Børnekor <br>
-        //             20:30 - 22:00: <span class="sf-scene">[Store Scene]</span> Peter fra Næstved <br>
-        //             <br>
-        //             Lørdag<br><br>
-        //             07:00 - 09:00: Morgenmad <br>
-        //             09:00 - 11:00: AKTIVITET<br>
-        //             11:00 - 13:00: Frokost <br>
-        //             13:00 - 15:00: AKTIVITET<br>
-        //             17:30 - 19:00: Aftensmad <br>
-        //             19:00 - 20:00: <span class="sf-scene">[Store Scene]</span> Børnemetal med Frederik og Benjamin <br>
-        //             20:30 - 22:00: <span class="sf-scene">[Store Scene]</span> Michaels band <br>
-        //             <br>
-        //             Søndag<br><br>
-        //             07:00 - 09:00: Morgenmad <br>
-        //             09:00 - 17:00: Oprydning og nedpakning. <br>
-
-        //         </div>
-        //     </div>
-        // `;
     }
 }
